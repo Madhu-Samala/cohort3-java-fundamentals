8d0ce432224f92308e0f6a7648dcd20e36ab3164
MadhuSamala <madhusamala.trainer@gmail.com>
2022-10-04 12:24:56 +0100

    constructors-added

--- src/com/qa/java/oop/Employee.java
+++ src/com/qa/java/oop/Employee.java
@@ -9,5 +9,30 @@ public class Employee {
 	String department;
 	byte age;
 	char gender;
+	
+	//No-arg Constructor
+	Employee(){
+		System.out.println("Employee() constructor is called...");
+		id = 111;
+		name = "emp1";
+		salary = 34534.34f;
+		department = "development";
+		age = 25;
+		gender = 'M';
+	}
+
+	
+	
+	//Arg Constructor
+	
+	public Employee(int id, String name, float salary, String department, byte age, char gender) {	
+		System.out.println("Employee(....) arg construtor is invoked...");
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.department = department;
+		this.age = age;
+		this.gender = gender;
+	}
 
 }
--- src/com/qa/java/oop/Main.java
+++ src/com/qa/java/oop/Main.java
@@ -5,15 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		//create an object
 		Employee emp1;
-		emp1 = new Employee();
+		emp1 = new Employee(111,"emp1",345345.34f,"development",(byte)25,'M');
 		System.out.println(emp1.name);
 		System.out.println(emp1.salary);
-		emp1.id = 111;
-		emp1.name = "emp1";
-		emp1.department = "development";
-		emp1.age = 25;
-		emp1.salary = 34534.34f;
-		emp1.gender = 'M';
+		/*
+		 * emp1.id = 111; emp1.name = "emp1"; emp1.department = "development"; emp1.age
+		 * = 25; emp1.salary = 34534.34f; emp1.gender = 'M';
+		 */
 		
 		System.out.println("Id : " + emp1.id);
 		System.out.println("Name : " + emp1.name);
@@ -23,15 +21,13 @@ public class Main {
 		System.out.println("Gender : " + emp1.gender);
 		
 		Employee emp2;
-		emp2 = new Employee();
+		emp2 = new Employee(222,"emp2",445345.34f,"QA",(byte)26,'F');
 		
 		System.out.println(emp2.name);
-		emp2.id = 222;
-		emp2.name = "emp2";
-		emp2.department = "QA";
-		emp2.salary = 564645.3f;
-		emp2.age = 30;
-		emp2.gender = 'F';
+		/*
+		 * emp2.id = 222; emp2.name = "emp2"; emp2.department = "QA"; emp2.salary =
+		 * 564645.3f; emp2.age = 30; emp2.gender = 'F';
+		 */
 		
 		System.out.println("Id : " + emp2.id);
 		System.out.println("Name : " + emp2.name);
